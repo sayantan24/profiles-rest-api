a841a7f9a2f965c6adc3084720456244866842a4
Sayantan <pradhan.sayantan24@gmail.com>
2020-08-28 10:20:39 +0530

    *djangoadmin*

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -11,16 +11,18 @@ class UserProfileManager(BaseUserManager):
         if not email:
             raise ValueError('user must have an email address')
 
-            email = self.normalize_email(email)
-            user = self.model(email=email,name=name)
-            user.set_password(password)
-            user.save(using=self._db)
+        email = self.normalize_email(email)
+        user = self.model(email=email,name=name,)
+        user.set_password(password)
+        user.save(using=self._db)
 
-            return user
+        return user
 
-    def create_super_user(self,email,name,password):
+    def create_superuser(self,email,name,password):
         """Create and save super user"""
-        user = self.create_user(email,name,paasword)
+        user = self.create_user(email,name,password)
+
+
         user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
