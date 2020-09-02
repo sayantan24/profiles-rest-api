7745e7b1f637d7b350c2656b2d31f9142657d61f
Sayantan <pradhan.sayantan24@gmail.com>
2020-09-02 10:34:57 +0530

    *configuredfordeployment*

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2zw3$)9w9g+i*u%k)k(e1y*b7&g3*xr5c=w)0cv8chnc&n02*3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG',1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
